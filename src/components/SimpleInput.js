f0741e452771e447e95b451b795bc7b2e648b28d
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-26 17:27:20 +0300

    Add formIsValid state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      setFormIsValid(true);
+    } else {
+      setFormIsValid(false);
+    }
+  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
-  const nameInputBlurHandler = () => {
+  const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
   };
 
@@ -18,17 +29,15 @@ const SimpleInput = (props) => {
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
 
-    setEnteredNameIsValid(true);
-
     console.log(enteredName);
-  };
 
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+    setEnteredName('');
+    setEnteredNameTouched(false);
+  };
 
   const nameInputClasses = nameInputIsInvalid
     ? 'form-control invalid' 
@@ -47,7 +56,7 @@ const SimpleInput = (props) => {
         {nameInputIsInvalid && <p className='error-text'>Field must not be empty</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
